9e34f9f2c6cf69cc0604187a98c76e575e57b240
clear93 <736512612@qq.com>
2018-12-28 11:29:38 +0800

    'change'

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ XMLHttpRequest demo
  4. XMLHttpRequest.setRequestHeader() 是设置HTTP请求头部的方法。此方法必须在  open() 方法和 send()   之间调用。
  5. XMLHttpRequest.send() 方法用于发送 HTTP 请求。如果是异步请求（默认为异步请求），则此方法会在请求发送后立即返回；如果是同步请求，则此方法直到响应到达后才会返回。XMLHttpRequest.send() 方法接受一个可选的参数，其作为请求主体；如果请求方法是 GET 或者 HEAD，则应将请求主体设置为 null。
  6. XMLHttpRequest.onreadystatechange 会在 XMLHttpRequest 的readyState 属性发生改变时触发 readystatechange 事件的时候被调用。警告：这个方法不该用于同步的requests对象,并且不能在内部(C++)代码中使用.
+ 
+ 
+ 
--- XMLHttpRequest.js
+++ XMLHttpRequest.js
@@ -1,3 +1,4 @@
+/*
 var request;
 if (window.XMLHttpRequest) { // Mozilla, Safari, ...
 	request = new XMLHttpRequest();
@@ -24,4 +25,48 @@ request.onreadystatechange = function() {
 			console.log(request.responseText)
 		}
 	}
-};
+};*/
+
+var xhr;
+if (window.XMLHttpRequest) {　 // Mozilla, Safari...
+	xhr = new XMLHttpRequest();
+} else if (window.ActiveXObject) { // IE
+	try {
+		xhr = new ActiveXObject('Msxml2.XMLHTTP');
+	} catch (e) {
+		try {
+			xhr = new ActiveXObject('Microsoft.XMLHTTP');
+		} catch (e) {}
+	}
+}
+if (xhr) {
+	xhr.onreadystatechange = onReadyStateChange;
+	// POST示例
+	xhr.open('POST', 'http://jsonplaceholder.typicode.com/posts', true);
+	// 设置 Content-Type 为 application/x-www-form-urlencoded
+	// 以表单的形式传递数据
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.send('username=admin&password=root');
+	
+	// GET示例：
+	/*xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/1', true);
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.send(null);*/
+}
+
+// onreadystatechange 方法
+function onReadyStateChange() {
+	// 该函数会被调用四次
+	console.log(xhr.readyState);
+	if (xhr.readyState === 4) {
+		// everything is good, the response is received
+		if (xhr.status === 200) {
+			console.log(xhr.responseText);
+		} else {
+			console.log('There was a problem with the request.');
+		}
+	} else {
+		// still not ready
+		console.log('still not ready...');
+	}
+}
